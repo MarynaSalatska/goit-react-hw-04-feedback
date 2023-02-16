963cdc8a56e9e6023abc2bc23c1654782006743e
Maryna <yasinskamarina@gmail.com>
2023-02-16 16:28:20 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback()
+      ? Math.ceil((this.state.good / this.countTotalFeedback()) * 100)
+      : 0;
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+export function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div
+      class="buttons"
+      style={{ display: 'flex', gap: '20px', borderRadius: '5px' }}
+    >
+      {options.map(item => (
+        <button key={item} name={item} type="button" onClick={onLeaveFeedback}>
+          {item}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+//в апп onLeaveFeedback = e => {
+//   const { name } = e.target;
+//   this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+// switch (name) {
+//   case 'good':
+//     this.setState(prevState => ({ good: prevState.good + 1 }));
+//     break;
+//   case 'neutral':
+//     this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+//     break;
+//   case 'bad':
+//     this.setState(prevState => ({ bad: prevState.bad + 1 }));
+//     break;
+
+//   default:
+//     break;
+// }
+// };
